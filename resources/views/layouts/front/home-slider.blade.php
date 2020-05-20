9ace3c9d68de06cb756f14a85b54b849cadc6c9d
vagrant <vagrant@homestead>
2020-05-20 22:35:06 +0000

    :v commit

---
+++ resources/views/layouts/front/home-slider.blade.php
@@ -0,0 +1,15 @@
+<section id="hero" class="hero-section top-area">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="hero-content">
+                    <h1 class="hero-title">{{config('app.name')}} Holiday <br> Collection</h1>
+                    <p class="hero-text">Amazing e-commerce web app for <strong class="text-success">!</strong></p>
+                    <a class="btn btn-success" href="https://wa.me/543541623622" target="_blank" role="button">Contacto <i class="fa fa-cloud-download"></i></a>
+                    <a class="btn btn-danger" href="https://wa.me/543541623622" target="_blank" role="button">Contacto <i class="fa fa-paper-plane"></i></a>
+                </div>
+            </div>
+            <div class="col-md-4"></div>
+        </div>
+    </div>
+</section>
